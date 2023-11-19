6cabf3b333bc6478b158bc59b408af34747d039b
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-11-19 13:12:03 +0200

    Add dummy Data to test

---
+++ StoreManagement/Controllers/UsersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static List<string> _users = new List<string>() { "Ali", "Qdri", "Fouad", "Saad" };
+        [HttpGet]
+        public List<string> GetUsers()
+        {
+            return _users;
+        }
+
+        [HttpGet("{id}")]
+        public string GetUsers(int id)
+        {
+            if (id >= 0 && id < _users.Count)
+            {
+
+                return _users[id];
+            }
+            return "";
+        }
+
+        [HttpPost]
+
+        public void AddUsers(string userName)
+        {
+
+            _users.Add(userName);
+        }
+
+        [HttpPut("{id}")]
+
+        public void UpdateUser(int id, string userName)
+        {
+            if(id >= 0 && id < _users.Count) { _users[id] = userName; }
+        }
+
+        [HttpDelete("{id}")]
+
+        public void DeleteUser(int id)
+        {
+            if (id >= 0 && id < _users.Count) { _users.RemoveAt(id); }
+        }
+    }
+}
