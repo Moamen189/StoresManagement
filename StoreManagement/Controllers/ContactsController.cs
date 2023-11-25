a587b77eb0390e3ae157b669275a6d084358325e
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-11-25 14:26:03 +0200

    R ==> Contacts

---
+++ StoreManagement/Controllers/ContactsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Services;
+
+namespace StoreManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ContactsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts() { 
+        
+            var contacts = context.Contacts.ToList();
+
+            return Ok(contacts);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetContact(int id)
+        {
+            var contact = context.Contacts.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+        //[HttpPost]
+    }
+}
