8d3e5bd5713e07d9566d3f47b3488c3cff435697
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-11-27 11:13:21 +0200

    UpdateProductDto

---
+++ StoreManagement/Models/ProductDto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.Models
+{
+    public class ProductDto
+    {
+        [Required, MaxLength(100)]
+
+        public string Name { get; set; } = "";
+        [MaxLength(4000)]
+
+        public string? Description { get; set; }
+        [Required,MaxLength(100)]
+
+        public string Category { get; set; } = "";
+        [Required,MaxLength(100)]
+
+        public string Brand { get; set; } = "";
+        [Required]
+        public decimal Price { get; set; }
+       
+
+        public IFormFile? ImageFileName { get; set; }
+    }
+}
