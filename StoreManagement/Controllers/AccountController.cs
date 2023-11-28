ffb3698626854c594418b2725c5604911487b333
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-11-28 11:35:01 +0200

    Update

---
+++ StoreManagement/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Models;
+using System.Security.Claims;
+
+namespace StoreManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private string CreateJwtToken(User user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim("id" , "" + user.Id),
+                new Claim("role" , user.Role)
+
+            };
+
+        }
+    }
+}
