33903a160eab4d9705a8e9d8aefaa6129783daf7
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-11-28 14:23:56 +0200

    Update UserDto , |UserProfileDto

--- StoreManagement/Models/UserProfileDto.cs
+++ StoreManagement/Models/UserProfileDto.cs
@@ -2,27 +2,27 @@
 
 namespace StoreManagement.Models
 {
-    public class UserDto
+    public class UserProfileDto
     {
-        [Required, MaxLength(100)]
+        public int Id { get; set; }
+      
         public string firstName { get; set; } = "";
-        [Required, MaxLength(100)]
+ 
 
         public string lastName { get; set; } = "";
-        [Required,EmailAddress ,  MaxLength(100)]
+      
 
         public string Email { get; set; } = "";
-        [Required,MinLength(8), MaxLength(100)]
 
-
-        public string Password { get; set; } = "";
-        [Required,MaxLength(20)]
 
         public string Phone { get; set; } = "";
-        [MaxLength(100)]
+        
 
         public string Address { get; set; } = "";
+    
 
+        public string Role { get; set; } = "";
 
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
 }
