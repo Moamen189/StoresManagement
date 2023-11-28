20fd9ce74b1570b461dd2def4a0c6ff9a7022ece
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-11-28 12:11:43 +0200

    Create Jwt

--- StoreManagement/Controllers/AccountController.cs
+++ StoreManagement/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using StoreManagement.Models;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
 
 namespace StoreManagement.Controllers
 {
@@ -9,6 +12,12 @@ namespace StoreManagement.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IConfiguration configuration;
+
+        public AccountController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         private string CreateJwtToken(User user)
         {
             List<Claim> claims = new List<Claim>()
@@ -18,6 +27,18 @@ namespace StoreManagement.Controllers
 
             };
 
+            string secretKey = configuration["JwtSettings:Key"]!;
+            var SmentricKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var signInCredetial = new SigningCredentials(SmentricKey, SecurityAlgorithms.HmacSha256);
+            var token = new JwtSecurityToken(
+                issuer: configuration["JwtSettings:Issuer"],
+                audience :configuration["JwtSettings:Audience"],
+                claims:claims,
+                expires:DateTime.Now.AddDays(1),
+                signingCredentials: signInCredetial
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            return jwt;
         }
     }
 }
