f7e348ec8f1cc0277330cf37b6ec2706def7710b
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-11-29 13:39:17 +0200

    apply swagger

--- StoreManagement/Program.cs
+++ StoreManagement/Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using StoreManagement.Filters;
 using StoreManagement.Middleware;
 using StoreManagement.Services;
@@ -16,7 +17,34 @@ namespace StoreManagement
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "My API",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+   {
+     new OpenApiSecurityScheme
+     {
+       Reference = new OpenApiReference
+       {
+         Type = ReferenceType.SecurityScheme,
+         Id = "Bearer"
+       }
+      },
+      new string[] { }
+    }
+  });
+            });
             builder.Services.AddDbContext<ApplicationDbContext>(options => {
                 var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
                 options.UseSqlServer(ConnectionString); 
