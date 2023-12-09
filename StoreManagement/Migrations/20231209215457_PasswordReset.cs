d02a16270fdeafa2285e551e4d9bde3d15fbbd0d
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-12-09 23:56:03 +0200

    UpdateDataBase

---
+++ StoreManagement/Migrations/20231209215457_PasswordReset.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordReset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PasswordResets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Token = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordResets", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PasswordResets_Email",
+                table: "PasswordResets",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PasswordResets");
+        }
+    }
+}
