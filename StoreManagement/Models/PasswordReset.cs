b18ae1b3c948be3f6dd0ff1961981fee2543be00
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-12-09 23:53:36 +0200

    PasswordReset

---
+++ StoreManagement/Models/PasswordReset.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.Models
+{
+    [Index("Email" , IsUnique = true)]
+    public class PasswordReset
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Email { get; set; } = "";
+        [MaxLength(100)]
+
+        public string Token { get; set; } = "";
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
