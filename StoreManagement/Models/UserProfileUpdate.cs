5c44fa590193223b20273c01f785bf06ba612070
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-12-20 12:27:31 +0200

    UserProfileUpdate for update user profile

---
+++ StoreManagement/Models/UserProfileUpdate.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.Models
+{
+    public class UserProfileUpdate
+    {
+        [Required, MaxLength(100)]
+        public string firstName { get; set; } = "";
+        [Required, MaxLength(100)]
+
+        public string lastName { get; set; } = "";
+        [Required, EmailAddress, MaxLength(100)]
+
+        public string Email { get; set; } = "";
+        [Required, MaxLength(20)]
+
+        public string Phone { get; set; } = "";
+        [MaxLength(100)]
+
+        public string Address { get; set; } = "";
+    }
+}
