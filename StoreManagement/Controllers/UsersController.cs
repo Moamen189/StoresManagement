fe32591cc1df82264a86bce5427785163992ebd3
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-12-20 15:47:44 +0200

    Apply Users controllers

---
+++ StoreManagement/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    public class UsersController : ControllerBase
+    {
+    }
+}
