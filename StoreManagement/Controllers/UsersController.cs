7584ea08d47eba7227b399a6a04c60f46ecbf635
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-12-20 16:06:20 +0200

    Get users

--- StoreManagement/Controllers/UsersController.cs
+++ StoreManagement/Controllers/UsersController.cs
@@ -1,13 +1,43 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Models;
+using StoreManagement.Services;
 
 namespace StoreManagement.Controllers
 {
+    [Authorize(Roles = "admin")]
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(Roles = "admin")]
     public class UsersController : ControllerBase
     {
+        private readonly ApplicationDbContext context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers() {
+         var users = context.Users.OrderByDescending(u => u.Id).ToList();
+         List<UserProfileDto> userProfileDto = new List<UserProfileDto>();
+        foreach (var user in users)
+            {
+                var userProfiles = new UserProfileDto()
+                {
+                    Id = user.Id,
+                    firstName = user.firstName, 
+                    lastName = user.lastName,
+                    Email = user.Email,
+                    Address = user.Address,
+                    Role = user.Role,
+                    CreatedAt = user.CreatedAt
+                };
+                userProfileDto.Add(userProfiles);
+            }
+        return Ok(userProfileDto);
+        }
     }
 }
+ 
