7cc0ad506db04da94e159649032fbbd3dcfadcd2
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-12-21 15:16:52 +0200

    Cartsummary

---
+++ StoreManagement/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Services;
+
+namespace StoreManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCart(string ProductIdentifiers) {
+            var ProductDictionary = OrderHelper.GetProductDictionary(ProductIdentifiers);
+            return Ok(ProductDictionary);
+        
+        }
+    }
+}
