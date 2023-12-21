325b2d7255664c0794f299173d52abbbc888448e
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-12-21 11:02:30 +0200

    Update jwtreader

---
+++ StoreManagement/Services/JwtReader.cs
@@ -0,0 +1,36 @@
+﻿using StoreManagement.Models;
+using System.Security.Claims;
+
+namespace StoreManagement.Services
+{
+    public class JwtReader
+    {
+        public static int getUserId(ClaimsPrincipal user)
+        {
+            int id;
+            var Identity = user.Identity as ClaimsIdentity;
+            if (Identity == null)
+            {
+                return -1;
+
+            }
+            var claim = Identity.Claims.FirstOrDefault(c => c.Type.ToLower() == "id");
+
+            if (claim == null)
+            {
+                return -1;
+
+            }
+
+            try
+            {
+                id = int.Parse(claim.Value);
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+            return id;
+        }
+    }
+}
