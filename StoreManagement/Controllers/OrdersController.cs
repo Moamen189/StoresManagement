331ad7f7c36e15c22f13ed53958a81e1f917fc57
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-12-22 19:25:54 +0200

    UpdateOrder

---
+++ StoreManagement/Controllers/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreManagement.Services;
+
+namespace StoreManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+    }
+}
---
+++ StoreManagement/Models/OrderDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StoreManagement.Models
+{
+    public class OrderDto
+    {
+        [Required]
+        public string ProductIdentifiers { get; set; } = "";
+        [Required , MinLength(30) , MaxLength(100)]
+
+        public string DeliveryAddress { get; set; } = "";
+        [Required]
+
+        public string PaymentMethod { get; set; } = "";
+    }
+}
