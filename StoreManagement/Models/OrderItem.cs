b2f08e42a432375a82677eab4bdf554e14a464fd
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-12-22 18:07:33 +0200

    Update Order Item

--- StoreManagement/Models/OrderItem.cs
+++ StoreManagement/Models/OrderItem.cs
@@ -1,6 +1,18 @@
-﻿namespace StoreManagement.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StoreManagement.Models
 {
     public class OrderItem
     {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        [Precision(16 , 2)]
+        public decimal unitPrice { get; set; }
+
+        public Order Order { get; set; }
+        public Product Product { get; set; } = null!;
     }
 }
