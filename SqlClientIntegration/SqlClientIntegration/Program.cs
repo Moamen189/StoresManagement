48d2e51b808f715573815fc068226c81491a5005
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-12-25 11:11:04 +0200

    Debugg With Loagging

--- SqlClientIntegration/SqlClientIntegration/Program.cs
+++ SqlClientIntegration/SqlClientIntegration/Program.cs
@@ -6,7 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddLogging(cfg =>
+{
+    cfg.AddDebug();
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
